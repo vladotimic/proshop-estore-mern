42e5c34fbe2b7c45f01399a60aa95e0aa9c01652
Vladislav Timić <32008359+vladotimic@users.noreply.github.com>
2022-03-04 16:40:38 +0100

    clean up backend routes with controllers

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,30 +1,12 @@
 import express from 'express';
-import { StatusCodes } from 'http-status-codes';
+import {
+  getAllProducts,
+  getSingleProduct,
+} from '../controllers/productController.js';
+
 const router = express.Router();
-import Product from '../models/Product.js';
 
-// @desc    Fetch all products
-// @route   GET /api/products
-// @access  Public
-router.get('/', async (req, res) => {
-  const products = await Product.find({});
-  res.status(StatusCodes.OK).json({
-    length: products.length,
-    products,
-  });
-});
-
-// @desc    Fetch single product
-// @route   GET /api/products/:id
-// @access  Public
-router.get('/:id', async (req, res) => {
-  const { id } = req.params;
-  const product = await Product.findById(id);
-  if (!product) {
-    res.status(StatusCodes.NOT_FOUND);
-    throw new Error('Product not found');
-  }
-  res.status(StatusCodes.OK).json({ product });
-});
+router.route('/').get(getAllProducts);
+router.route('/:id').get(getSingleProduct);
 
 export default router;
